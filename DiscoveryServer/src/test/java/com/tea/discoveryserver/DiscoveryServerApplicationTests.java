ac1f7d9532d089c84df82b231dfa547d56e04d67
tea.nguyen <tea.nguyen@vincere.io>
2023-01-22 00:16:59 +0700

    Set up Microservices

---
+++ DiscoveryServer/src/test/java/com/tea/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tea.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
